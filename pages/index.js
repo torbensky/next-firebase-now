b9cef2be0bb430b2f804961575eaf7e6381f5c28
Torben Werner <torbensky@users.noreply.github.com>
2018-12-26 11:15:51 -0800

    Basic auth example

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import firebase from "firebase/app";
+import firebase from "firebase/app"
+import "firebase/auth"
 
 // Initialize Firebase
 // TODO: Replace with your project's customized code snippet
@@ -10,9 +11,30 @@ var config = {
 };
 firebase.initializeApp(config);
 
+const login = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}
+
 export default () => {
     return <div>
         <p>Hello, firebase</p>
         <p>App: {firebase.app.name}</p>
+        <button onClick={login}>Auth</button>
     </div>
 }
--- readme.md
+++ readme.md
@@ -3,11 +3,16 @@
 I created this to make a "vanilla" Now/Firebase example because I can't seem to get an app that uses firebase to deploy to the Now 2.0 platform. After seeing some posts in spectrum.chat on the subject I decided to make a basic repo with the goal of providing an example for those who want to see source.
 
 ## Current status: 
-I get broken deployments and cannot run on Zeit Now 2.0. I can run locally using `dev` script.
+- Client app loads after switching import approach to using submodules.
+- Server `firebase-admin` is unproven. Need to setup routing to get this demonstrated.
 
-## Goal: 
-Get an app that uses `firebase` and `firebase-admin` libraries to deploy on Now 2.0.
+## Goals: 
+~~Get an app that uses `firebase` and `firebase-admin` libraries to deploy on Now 2.0.~~
+
+- Using other firebase client submodules (~~auth,~~ storage, database, firestore)
+
+- Using the admin library (the current example doesn't actually do any server stuff yet)
 
 ## TLDR;
 - `yarn dev` works, I get an app
-- `now` results in borked deploy
+- `now` works, I get an "app"
